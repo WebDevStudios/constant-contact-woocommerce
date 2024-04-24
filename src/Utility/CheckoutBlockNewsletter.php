65472c78efeb884604715d9a6bcc30dfa57be7c7
Michael Beckwith <tw2113@gmail.com>
2024-04-24 08:50:59 -0500

    check for functions for both experimental and future available. Refactor to have a method dedicated to block args

--- src/Utility/CheckoutBlockNewsletter.php
+++ src/Utility/CheckoutBlockNewsletter.php
@@ -24,28 +24,31 @@ class CheckoutBlockNewsletter {
 	public static $namespace = 'wc/cc-woo';
 
 	public function add_newsletter_to_checkout_block() {
+
+		$block_args = $this->get_newsletter_checkout_block_args();
+		if ( function_exists( 'woocommerce_blocks_register_checkout_field' ) ) {
+			woocommerce_blocks_register_checkout_field(
+				$block_args
+			);
+		} elseif ( function_exists( '__experimental_woocommerce_blocks_register_checkout_field' ) ) {
+			__experimental_woocommerce_blocks_register_checkout_field(
+				$block_args
+			);
+		}
+	}
+
+	private function get_newsletter_checkout_block_args() {
 		$checkbox_location = get_option( 'cc_woo_store_information_checkbox_location', 'woocommerce_after_checkout_billing_form' );
 		$location          = 'contact';
 		if ( $checkbox_location === 'woocommerce_after_checkout_billing_form' ) {
 			$location = 'address';
 		}
-		if ( function_exists( 'woocommerce_blocks_register_checkout_field' ) ) {
-			woocommerce_blocks_register_checkout_field(
-				[
-					'id'       => self::$namespace . '/newsletter-signup',
-					'label'    => 'Do you want to subscribe to our newsletter?',
-					'location' => $location,
-					'type'     => 'checkbox',
-				]
-			);
-		}
-		__experimental_woocommerce_blocks_register_checkout_field(
-			[
-				'id'       => self::$namespace . '/newsletter-signup',
-				'label'    => esc_html__( 'HIGH FIVE Sign me up to receive marketing emails', 'constant-contact-woocommerce' ),
-				'location' => $location,
-				'type'     => 'checkbox',
-			]
-		);
+
+		return [
+			'id'       => self::$namespace . '/newsletter-signup',
+			'label'    => 'Do you want to subscribe to our newsletter?',
+			'location' => $location,
+			'type'     => 'checkbox',
+		];
 	}
 }
