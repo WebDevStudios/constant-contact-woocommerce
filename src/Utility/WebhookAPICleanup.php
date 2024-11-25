d7e5e70aed631ab9f3453172423f2d9680145954
Michael Beckwith <tw2113@gmail.com>
2024-11-25 10:46:59 -0600

    update PHPDocs

--- src/Utility/WebhookAPICleanup.php
+++ src/Utility/WebhookAPICleanup.php
@@ -1,13 +1,42 @@
 <?php
+/**
+ * WooCommerce Webhook and API cleanup.
+ *
+ * @since   NEXT
+ * @author  WebDevStudios.
+ * @package cc-woo
+ */
 
 namespace WebDevStudios\CCForWoo\Utility;
 
+/**
+ * Webhook and API cleanup class.
+ *
+ * @since NEXT
+ */
 class WebhookAPICleanup {
 
+	/**
+	 * WPDB class instance.
+	 *
+	 * @var \WPDB $wpdb
+	 */
 	protected \WPDB $wpdb;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param \WPDB $wpdb
+	 */
 	public function __construct( \WPDB $wpdb) {
 		$this->wpdb = $wpdb;
 	}
+
+	/**
+	 * Clear out all of our webhooks by name.
+	 *
+	 * @since NEXT
+	 */
 	public function clear_webhooks() {
 		$query = "DELETE FROM {$this->wpdb->prefix}wc_webhooks WHERE topic = '%s'";
 		$result = $this->wpdb->query(
@@ -18,6 +47,11 @@ class WebhookAPICleanup {
 		);
 	}
 
+	/**
+	 * Clear out all of our API connections.
+	 *
+	 * @since NEXT
+	 */
 	public function clear_api_connections() {
 		$query  = "DELETE FROM {$this->wpdb->prefix}woocommerce_api_keys WHERE description LIKE 'Constant Contact - API%'";
 		$result = $this->wpdb->query(
